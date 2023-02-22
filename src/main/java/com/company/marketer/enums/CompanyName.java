5b826eb9d6bcb6925715cfe8921630af0acff639
Pahahentikys <pavelkemerovo.rus@gmail.com>
2023-02-23 00:12:50 +0300

    Feature: json-processing
    Added CompanyName enum

---
+++ src/main/java/com/company/marketer/enums/CompanyName.java
@@ -0,0 +1,15 @@
+package com.company.marketer.enums;
+
+
+/**
+ * Enum of company names for getting a data of price
+ */
+public enum CompanyName {
+    AAL,
+
+    AAME,
+
+    AAON,
+
+    AAPL
+}
