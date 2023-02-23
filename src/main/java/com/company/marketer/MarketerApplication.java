37b815c8d5a7066f61da1f217be1acc6d29a45f0
Pahahentikys <pavelkemerovo.rus@gmail.com>
2023-02-23 20:18:05 +0300

    Feature: storing-parsed-info-in-db
    Deleted unused imports from MarketerApplication

--- src/main/java/com/company/marketer/MarketerApplication.java
+++ src/main/java/com/company/marketer/MarketerApplication.java
@@ -1,17 +1,13 @@
 package com.company.marketer;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class MarketerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MarketerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MarketerApplication.class, args);
+    }
 
 }
