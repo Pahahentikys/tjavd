792fbe4617d18f4655d881b9960866e45722d15a
Pahahentikys <pavelkemerovo.rus@gmail.com>
2023-02-24 01:26:36 +0300

    Feature: storing-parsed-info-in-db
    Added for CompanyInfo entity @PrimaryKeyColumn attributes for date and name field

--- src/main/java/com/company/marketer/domain/CompanyInfo.java
+++ src/main/java/com/company/marketer/domain/CompanyInfo.java
@@ -3,7 +3,9 @@ package com.company.marketer.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.cql.Ordering;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 import org.springframework.data.cassandra.core.mapping.Table;
 
 import java.math.BigDecimal;
@@ -19,11 +21,12 @@ import java.util.UUID;
 
 @Table(value = "company_info")
 public class CompanyInfo {
-    @PrimaryKey
     private UUID uuid;
 
+    @PrimaryKeyColumn(name = "date", ordinal = 0, type = PrimaryKeyType.CLUSTERED, ordering = Ordering.DESCENDING)
     private LocalDate date;
 
+    @PrimaryKeyColumn(name = "name", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
     private String name;
 
     private BigDecimal closePrice;
