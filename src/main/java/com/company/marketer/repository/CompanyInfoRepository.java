e4eef963d14e707985a4c7748cdd8a435f420f4d
Pahahentikys <pavelkemerovo.rus@gmail.com>
2023-02-24 01:27:11 +0300

    Feature: storing-parsed-info-in-db
    CompanyInfoRepository updated. Added query: "findLastByName"

--- src/main/java/com/company/marketer/repository/CompanyInfoRepository.java
+++ src/main/java/com/company/marketer/repository/CompanyInfoRepository.java
@@ -4,9 +4,11 @@ package com.company.marketer.repository;
 import com.company.marketer.domain.CompanyInfo;
 import lombok.NonNull;
 import org.springframework.data.cassandra.repository.AllowFiltering;
+import org.springframework.data.cassandra.repository.Query;
 import org.springframework.data.cassandra.repository.ReactiveCassandraRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.UUID;
@@ -16,4 +18,10 @@ public interface CompanyInfoRepository extends ReactiveCassandraRepository<Compa
 
     @AllowFiltering
     Flux<List<CompanyInfo>> findByName(@NonNull String name);
+
+    @AllowFiltering
+    @Query(value = "select * from app_space.company_info" +
+            " where name = :name " +
+            " limit 1")
+    Mono<CompanyInfo> findLastByName(@NonNull String name);
 }
