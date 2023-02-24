dc56ba6de876d1b1499f0f40bf9493ef9e5caa41
Pahahentikys <pavelkemerovo.rus@gmail.com>
2023-02-24 17:33:21 +0300

    Feature: splunk-logging
    Added logger for SchedulerJobs

--- src/main/java/com/company/marketer/scheduler/SchedulerJobs.java
+++ src/main/java/com/company/marketer/scheduler/SchedulerJobs.java
@@ -3,6 +3,8 @@ package com.company.marketer.scheduler;
 import com.company.marketer.enums.CompanyName;
 import com.company.marketer.service.ImportJsonDataService;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Description;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -12,12 +14,20 @@ import java.util.Arrays;
 @Component
 @RequiredArgsConstructor
 public class SchedulerJobs {
+    Logger logger = LogManager.getLogger(SchedulerJobs.class);
+
     private final ImportJsonDataService importJsonDataService;
 
 
     @Description("Import json data in data base")
     @Scheduled(cron = "${marketer.scheduler.cron.import-json}")
     public void importJsonInfoIntoDataBase() {
-        Arrays.stream(CompanyName.values()).forEach(cn -> importJsonDataService.importDataByCompanyName(cn).blockOptional());
+        Arrays.stream(CompanyName.values()).forEach(cn -> {
+            logger.info("[SchedulerJobs.importJsonInfoIntoDataBase] Started scheduled import json data for company: %s".formatted(cn));
+
+            importJsonDataService.importDataByCompanyName(cn)
+                    .doOnSuccess(d -> logger.info("[SchedulerJobs.importJsonInfoIntoDataBase] Ended scheduled import json data for company: %s".formatted(cn)))
+                    .blockOptional();
+        });
     }
 }
